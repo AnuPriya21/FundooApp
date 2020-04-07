b75687abf98d197c1aba75a3b3c4a08e4b50dd43
AnuPriya21 <anupriyam2105@gmail.com>
2020-04-07 21:24:33 +0530

    [Anu Priya] Add. Models file is used to write Class based Models for our Django Applications

---
+++ project/fundoo/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django import forms
+
+class Registration(models.Model):
+    name = models.CharField(max_length = 100)
+    username = models.CharField(max_length = 100)
+    email = models.EmailField()
+    password = models.CharField(max_length = 100)
+    password1 = models.CharField(max_length = 100)
+
+    
+    def __str__(self):
+        return self.id
