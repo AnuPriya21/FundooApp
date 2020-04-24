8915361cafda09ad5bf917d5135cd3b038348373
AnuPriya21 <anupriyam2105@gmail.com>
2020-04-24 12:38:21 +0530

    [Anu Priya] Refacor. Note class for CRUD operations

--- project/fundoo/admin.py
+++ project/fundoo/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
-from .models import Registration
+from .models import Registration, Upload, Notes
+
 
 admin.site.register(Registration)
+admin.site.register(Upload)
+admin.site.register(Notes)
+
--- project/fundoo/models.py
+++ project/fundoo/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django import forms
+from django.contrib.auth.models import User
+
 
 class Registration(models.Model):
     name = models.CharField(max_length = 100)
@@ -7,7 +9,6 @@ class Registration(models.Model):
     email = models.EmailField()
     password = models.CharField(max_length = 100)
     password1 = models.CharField(max_length = 100)
-
     
     def __str__(self):
         return self.id
@@ -15,3 +16,14 @@ class Registration(models.Model):
 class Upload(models.Model):
     uploaded_at = models.DateTimeField(auto_now_add=True)
     file = models.FileField()
+
+    def __str__(self):
+        return self.file
+
+class Notes(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=50)
+    text = models.TextField(max_length=200)
+    
+    def __str__(Self):
+        return self.title
