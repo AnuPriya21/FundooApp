fabea495595d0416c4943c73c89b1e8ff093f57a
AnuPriya21 <anupriyam2105@gmail.com>
2020-04-27 22:35:19 +0530

    [Anu Priya] Refactor. Refactored note model

--- project/fundoo/admin.py
+++ project/fundoo/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from .models import Registration, Upload, Notes
+from .models import Registration, Upload, Note
 
 
 admin.site.register(Registration)
 admin.site.register(Upload)
-admin.site.register(Notes)
+admin.site.register(Note)
 
