67a88934bbf413a08637917675c8e9819cd36799
AnuPriya21 <anupriyam2105@gmail.com>
2020-04-28 18:10:37 +0530

    [Anu Priya] Refactor. Refactored with label class

--- project/fundoo/admin.py
+++ project/fundoo/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from .models import Registration, Upload, Note
+from .models import Registration, Upload, Note, Label
 
 
 admin.site.register(Registration)
 admin.site.register(Upload)
 admin.site.register(Note)
-
+admin.site.register(Label)
